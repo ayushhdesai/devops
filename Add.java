6551c05143e3d4dfd53fc769443a1527b55026ba
Ayush Desai <56110330+ayushhdesai@users.noreply.github.com>
2022-04-28 09:58:50 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,18 @@
+import sys
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+sum=x+y
+print("The addition is :",sum)
+
+
+
+public class Add
+{  
+   public static void main(String args[])   
+ {  
+	int n1=Integer.parseInt(args[0]), n2=Integer.parseInt(args[1]), sum; 
+	System.out.println(n1 +" "+ n2); 
+	sum = n1 + n2;  
+	System.out.println("The sum of numbers is: "+sum);  
+ }  
+} 
